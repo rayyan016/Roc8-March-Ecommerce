530ba5ffeb8aa18d7c36b7115f8df601e78ff54f
Rayyan <rayyanaslam16@gmail.com>
2024-03-29 00:47:31 +0530

    Fix styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,48 +1,48 @@
 import Image from "next/image";
 
-import Icon from '@mdi/react';
-import { mdiChevronLeft, mdiChevronRight } from '@mdi/js';
+import Icon from "@mdi/react";
+import { mdiChevronLeft, mdiChevronRight } from "@mdi/js";
 
 import Search from "public/search.png";
 import Cart from "public/cart.png";
 
 const Navbar: React.FC = () => {
-    return (
-        <>
-            <div className="flex items-center justify-end space-x-4 mr-4 h-9">
-                <p className="text-[12px] font-normal">Help</p>
-                <p className="text-[12px] font-normal">Orders & Returns</p>
-                <p className="text-[12px] font-normal">Hi, John</p>
-            </div>
-            <div className="flex justify-between items-center ml-4 h-16">
-                <div className="text-[32px] font-bold">ECOMMERCE</div>
-                <div className="flex space-x-4 text-[16px] font-[600] -ml-40">
-                    <p>Categories</p>
-                    <p>Sale</p>
-                    <p>Clearance</p>
-                    <p>New Stock</p>
-                    <p>Trending</p>
-                </div>
-                <div className="icon flex space-x-4 mr-4">
-                    <div>
-                        <Image src={Search} alt="Image" width={32} height={32} />
-                    </div>
-                    <div>
-                        <Image src={Cart} alt="Image" width={32} height={32} />
-                    </div>
-                </div>
-            </div>
-            <div className="bg-[#F4F4F4] flex items-center justify-center space-x-3 h-9">
-                <div>
-                    <Icon path={mdiChevronLeft} size={0.67} />
-                </div>
-                <div>Get 10% off on business sign up</div>
-                <div>
-                    <Icon path={mdiChevronRight} size={0.67} />
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="mr-4 flex h-9 items-center justify-end space-x-4">
+        <p className="text-[12px] font-normal">Help</p>
+        <p className="text-[12px] font-normal">Orders & Returns</p>
+        <p className="text-[12px] font-normal">Hi, John</p>
+      </div>
+      <div className="ml-4 flex h-16 items-center justify-between">
+        <div className="text-[32px] font-bold">ECOMMERCE</div>
+        <div className="-ml-40 flex space-x-4 text-[16px] font-[600]">
+          <p>Categories</p>
+          <p>Sale</p>
+          <p>Clearance</p>
+          <p>New Stock</p>
+          <p>Trending</p>
+        </div>
+        <div className="icon mr-4 flex space-x-4">
+          <div>
+            <Image src={Search} alt="Image" width={32} height={32} />
+          </div>
+          <div>
+            <Image src={Cart} alt="Image" width={32} height={32} />
+          </div>
+        </div>
+      </div>
+      <div className="flex h-9 items-center justify-center space-x-3 bg-[#F4F4F4]">
+        <div>
+          <Icon path={mdiChevronLeft} size={0.67} />
+        </div>
+        <div>Get 10% off on business sign up</div>
+        <div>
+          <Icon path={mdiChevronRight} size={0.67} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
