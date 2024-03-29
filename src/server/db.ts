ac6d5d33368b10ed500dcfffbc3581062a994a49
Rayyan <rayyanaslam16@gmail.com>
2024-03-29 20:22:40 +0530

    Remove boilerplate

--- src/server/db.ts
+++ src/server/db.ts
@@ -6,6 +6,11 @@ const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    }
   });
 
 const globalForPrisma = globalThis as unknown as {
@@ -14,4 +19,4 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
